5043b8d4c460bf9059e134ed3a9e0cdf16a2ba90
Nicholas Johnson <NicholasJohnson9149@users.noreply.github.com>
2017-07-24 11:47:02 -0700

    Added an empty project

---
+++ empty_CC1310_LAUNCHXL_tirtos_ccs/Debug/ccfg.d
@@ -0,0 +1,19 @@
+# FIXED
+
+ccfg.obj: ../ccfg.c
+ccfg.obj: /Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/ccfg.c
+ccfg.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h
+ccfg.obj: /Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_types.h
+ccfg.obj: /Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdbool.h
+ccfg.obj: /Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/../inc/hw_chip_def.h
+ccfg.obj: /Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_ccfg.h
+ccfg.obj: /Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_ccfg_simple_struct.h
+
+../ccfg.c: 
+/Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/ccfg.c: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdint.h: 
+/Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_types.h: 
+/Applications/ti/ccsv7/tools/compiler/ti-cgt-arm_17.3.0.STS/include/stdbool.h: 
+/Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/../inc/hw_chip_def.h: 
+/Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_ccfg.h: 
+/Applications/ti/simplelink_cc13x0_sdk_1_30_00_06/source/ti/devices/cc13x0/startup_files/../inc/hw_ccfg_simple_struct.h: 
---
+++ empty_CC1310_LAUNCHXL_tirtos_ccs/Debug/subdir_vars.mk
@@ -0,0 +1,45 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+CMD_SRCS += \
+../CC1310_LAUNCHXL_TIRTOS.cmd 
+
+C_SRCS += \
+../CC1310_LAUNCHXL.c \
+../ccfg.c \
+../empty.c \
+../main_tirtos.c 
+
+C_DEPS += \
+./CC1310_LAUNCHXL.d \
+./ccfg.d \
+./empty.d \
+./main_tirtos.d 
+
+OBJS += \
+./CC1310_LAUNCHXL.obj \
+./ccfg.obj \
+./empty.obj \
+./main_tirtos.obj 
+
+OBJS__QUOTED += \
+"CC1310_LAUNCHXL.obj" \
+"ccfg.obj" \
+"empty.obj" \
+"main_tirtos.obj" 
+
+C_DEPS__QUOTED += \
+"CC1310_LAUNCHXL.d" \
+"ccfg.d" \
+"empty.d" \
+"main_tirtos.d" 
+
+C_SRCS__QUOTED += \
+"../CC1310_LAUNCHXL.c" \
+"../ccfg.c" \
+"../empty.c" \
+"../main_tirtos.c" 
+
+
